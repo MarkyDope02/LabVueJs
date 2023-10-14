e51b98dd56332c27ba1160abcc7220d40c6c40b2
sedi <145019257+cydieg@users.noreply.github.com>
2023-10-14 22:46:15 +0800

    weeee

---
+++ labvue/src/views/aboutUs.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>Welcome to Car Shop</h1>
+  </header>
+  <div class="container">
+    <h2>Your Destination for Cars</h2>
+    <p>
+      Welcome to Car Shop, your one-stop destination for all things cars. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac nunc et mi vehicula congue at ac massa. Quisque ut libero et tellus finibus auctor. Fusce venenatis, purus id bibendum tincidunt, urna elit elementum nunc, vel lacinia justo purus id purus.
+    </p>
+    <p>
+      Explore our wide selection of cars and discover the beauty of these incredible vehicles. Nunc efficitur, ex eget malesuada hendrerit, libero turpis mattis justo, sit amet ullamcorper erat mi quis dui. Curabitur sit amet sem vel nisi dictum iaculis ac ut quam.
+    </p>
+  </div>
+  <footer>
+    &copy; 2023 Car Shop
+  </footer>
+</template>
+
+
+
+<script>
+export default {
+  name: 'LogInPage'
+}
+</script>
+
+<style>
+@import '../assets/assets/css/about.css';
+</style>
